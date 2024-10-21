c1313d97c941e423c96e46b090740024b8c2e070
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-10-21 20:04:48 +0300

    define toast notification feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
@@ -14,6 +16,7 @@ import Footer from './components/Footer'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
